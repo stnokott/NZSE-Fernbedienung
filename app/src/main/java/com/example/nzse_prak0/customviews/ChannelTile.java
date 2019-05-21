41ea25c8833246e8c9ae60ece39577effbd64f3b
NoahK <nftrash@web.de>
2019-05-21 13:14:09 +0200

    Ungenutztes Import-Statement entfernt

--- app/src/main/java/com/example/nzse_prak0/customviews/ChannelTile.java
+++ app/src/main/java/com/example/nzse_prak0/customviews/ChannelTile.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.graphics.drawable.Animatable;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
