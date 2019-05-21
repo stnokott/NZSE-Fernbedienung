a37d5e786ff8577773a9071c974eef0a3254ddf0
NoahK <nftrash@web.de>
2019-05-21 10:40:10 +0200

    Redundante LottieAnimationView entfernt

--- app/src/main/java/com/example/nzse_prak0/Activity_SwitchedOff.java
+++ app/src/main/java/com/example/nzse_prak0/Activity_SwitchedOff.java
@@ -2,12 +2,10 @@ package com.example.nzse_prak0;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Activity_SwitchedOff extends AppCompatActivity {
@@ -22,18 +20,8 @@ public class Activity_SwitchedOff extends AppCompatActivity {
         btnSwitchOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final LottieAnimationView btnSwitchLottie = findViewById(R.id.lottieViewPower);
-                btnSwitchLottie.setVisibility(View.VISIBLE);
-                btnSwitchLottie.playAnimation();
-
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        startActivity(new Intent(Activity_SwitchedOff.this, Activity_ChooseChannel.class));
-                        finish(); // verhindert, dass man aus Activity_SwitchedOn per Back-Button zurück gehen kann
-                    }
-                }, 2000);
+                startActivity(new Intent(Activity_SwitchedOff.this, Activity_ChooseChannel.class));
+                finish(); // verhindert, dass man aus Activity_SwitchedOn per Back-Button zurück gehen kann
             }
         });
     }
