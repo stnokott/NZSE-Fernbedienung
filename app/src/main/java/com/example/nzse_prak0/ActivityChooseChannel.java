28bf6a53338bfeb0fea4f6267c87fec3bdf944dc
Hung <chauhung21@gmail.com>
2019-05-24 14:48:45 +0200

    AsyncTask

--- app/src/main/java/com/example/nzse_prak0/ActivityChooseChannel.java
+++ app/src/main/java/com/example/nzse_prak0/ActivityChooseChannel.java
@@ -2,9 +2,12 @@ package com.example.nzse_prak0;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -12,7 +15,76 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.nzse_prak0.helpers.TileAdapter;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ActivityChooseChannel extends AppCompatActivity {
+
+    protected HttpRequest http = new HttpRequest("172.16.201.100", 5000, true);
+
+    ChannelManager channelManager = new ChannelManager();
+    ArrayList<Channel> listChannel;
+
+    private int lastPosition = 0;
+    private int maxPosition = 0;
+
+    // Channels
+    JSONObject jsonObject;
+
+
+    private class DownloadTask extends AsyncTask< Void, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(Void... params){
+            // resolving request and do not interrupt UI
+
+            // Asynchron Task for new ChannelManager
+            ChannelManager result = new ChannelManager();
+            try {
+                jsonObject= http.sendHttp("scanChannels=");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return jsonObject;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObj) {
+            if( listChannel != null){
+                listChannel.clear();
+                scanChannels(jsonObject);
+                Toast toast = Toast.makeText(getApplicationContext(), "Channels scanned!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast toast = Toast.makeText(getApplicationContext(), "Scanning channels...", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    void scanChannels(JSONObject jsonObj){
+
+        try {
+            channelManager.parseChannels(jsonObj);
+            maxPosition = channelManager.channelList.size();
+            http.sendHttp("channelMain="+ channelManager.channelList.get(lastPosition));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +95,15 @@ public class ActivityChooseChannel extends AppCompatActivity {
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(recyclerView.getContext(), 2);
         recyclerView.setLayoutManager(gridLayoutManager);
+
+        Button btnScanChannels = findViewById(R.id.btnScanChannels);
+        btnScanChannels.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                new DownloadTask().execute();
+            }
+        });
+
 
         String[] tileNames = {"Pro 7", "Kabeleins", "RTL", "ARD"};
         TileAdapter tileAdapter = new TileAdapter(tileNames);
