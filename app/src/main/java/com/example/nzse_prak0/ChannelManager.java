a533364bbb0999f625737666f7c4450dd8b6bdd4
NoahK <nftrash@web.de>
2019-05-24 13:37:23 +0200

    Klasse umbenannt, Attribute verändert

---
+++ app/src/main/java/com/example/nzse_prak0/ChannelManager.java
@@ -0,0 +1,33 @@
+package com.example.nzse_prak0;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ChannelManager {
+
+    public String response;
+
+    protected ArrayList<Channel> channelList = new ArrayList<>();
+
+    public void parseChannels(JSONObject json) throws JSONException {
+
+        if (json.has("channels")) {              // Überprüfe ob "channels" vorhanden ist
+
+            JSONArray channels = json.getJSONArray("channels");
+            response = json.getString("status");
+
+            for (int i = 0; i < channels.length(); i++) {
+                JSONObject channel = channels.getJSONObject(i);
+                Channel newChannel = new Channel(channel.getString("channel"), channel.getString("program"), channel.getString("provider"));
+                this.channelList.add(newChannel);
+                Log.d(newChannel.getProgram(), "Program");
+            }
+        }
+    }
+}
+
